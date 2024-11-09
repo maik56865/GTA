16311ad4b63109a6256ea211cbb0578af18490ab
maik56865 <142171083+maik56865@users.noreply.github.com>
2024-11-09 20:36:08 +0300

    movement fix

--- Assets/code/PlayerMovement.cs
+++ Assets/code/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody myRb;
     private Vector3 torque;
+    bool isGround;
     
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Ray ray = new Ray();
+        ray.origin = transform.position;
+        ray.direction = transform.up * -1;
+        isGround = Physics.Raycast(ray,0.5f);
         torque.y = Input.GetAxis("Mouse X") * Time.deltaTime * 100 ;
         velocity.x = Input.GetAxis("Horizontal") * speed ;
         velocity.y = myRb.velocity.y;
         velocity.z = Input.GetAxis("Vertical") * speed ;
-        if (Input.GetButtonDown("Jump")){
+        if (Input.GetButtonDown("Jump")&& isGround){
             velocity.y = speedJump;
         }
         myRb.velocity =  transform.rotation * velocity  ;
