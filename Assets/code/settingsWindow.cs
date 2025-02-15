907e9924a03438594df36f7c291c2e2c979ff831
maik56865 <142171083+maik56865@users.noreply.github.com>
2025-02-15 23:02:02 +0300

    mixer

---
+++ Assets/code/settingsWindow.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Audio;
+
+
+
+public class settingsWindow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource music;
+    
+    public Dropdown Language;
+    public TMP_InputField inputField;
+    public string newTXT;
+    public Slider MasterVolumeSlider;
+    public Slider MusicSlider;
+    public AudioMixer audioMixer;
+    public Slider SFX;
+    
+
+    void Start()
+    {
+        
+        newTXT = PlayerPrefs.GetString("TEXT");
+        inputField.text = newTXT;
+        MasterVolumeSlider.value = PlayerPrefs.GetFloat("volumeMusic");
+        SFX.value = PlayerPrefs.GetFloat("SFX");
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicSlider");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void volmeCH()
+    {
+        audioMixer.SetFloat("Master", MasterVolumeSlider.value);
+        PlayerPrefs.SetFloat("volumeMusic", MasterVolumeSlider.value);
+        audioMixer.SetFloat("Music", MusicSlider.value);
+        audioMixer.SetFloat("SFX", SFX.value);
+        PlayerPrefs.SetFloat("SFX", SFX.value);
+        PlayerPrefs.SetFloat("MusicSlider",music.volume);
+
+    }
+
+    public void inputF()
+    {
+        PlayerPrefs.SetString("TEXT", inputField.text.ToString());
+
+        
+    }
+
+    }
